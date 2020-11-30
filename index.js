6707f028412b578e049662836010ba687a0d4c76
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-30 21:20:34 +0700

    Download the url with image-downloader

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const Scraper = require("images-scraper");
+const download = require("image-downloader");
 
 const keyword = "banana";
-const limit = 100;
+const limit = 10;
+const destination = "/home/erik1997/Pictures/Reog";
 
 const google = new Scraper({ puppeteer: { headless: false } });
 
 (async () => {
   const results = await google.scrape(keyword, limit);
   results.forEach((e) => {
-    console.log(e.url);
+    const options = {
+      url: e.url,
+      dest: destination,
+    };
+
+    try {
+      const { filename } = download.image(options);
+      console.log("Saved to", filename);
+    } catch (err) {
+      console.log(err.message);
+    }
   });
 })();
