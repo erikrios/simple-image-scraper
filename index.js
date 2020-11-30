3d35b6e16af18563a6c7f6720c2f474385717c38
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-30 22:22:19 +0700

    Download the url with image-downloader

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const Scraper = require("images-scraper");
 const download = require("image-downloader");
 
-const keyword = "Banana";
-const limit = 10;
+const keyword = "Wallpaper Reog";
+const limit = 200;
 const destination = "/home/erik1997/Pictures/WallpaperReog/Reog";
 
 const google = new Scraper({ puppeteer: { headless: false } });
 
 (async () => {
   const results = await google.scrape(keyword, limit);
-  results.forEach(async (e) => {
-    const options = {
-      url: e.url,
-      dest: destination,
-    };
+  results.forEach(async (result, index) => {
+    const imageUrl = result.url;
+    const imageExtension = imageUrl.substr(imageUrl.length - 4);
 
-    try {
-      const { filename } = await download.image(options);
-      console.log("Saved to", filename);
-    } catch (err) {
-      console.log(err.message);
+    if (imageExtension.includes(".jpg") || imageExtension.includes(".png")) {
+      const options = {
+        url: imageUrl,
+        dest: `${destination}/image${index}${imageExtension}`,
+      };
+
+      try {
+        const { filename } = await download.image(options);
+        console.log("Saved to", filename);
+      } catch (err) {
+        console.log(err.message);
+      }
     }
   });
 })();
